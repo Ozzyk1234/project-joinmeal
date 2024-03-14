6d022e9a461454c946658e1710217799673f8890
Ozzyk1234 <bieleckioskar@gmail.com>
2024-03-14 18:28:08 +0100

    Respo

--- components/Button.jsx
+++ components/Button.jsx
@@ -1,11 +1,11 @@
 import Link from "next/link";
-import React, { useState } from "react";
+import React from "react";
 
 export default function Button({ name, action }) {
   return (
     <div>
       <Link href={action}>
-        <button className="bg-red-600 py-3 px-5 rounded-full hover:bg-white hover:text-red-600 transition-all duration-100">
+        <button className="bg-[#0A390C] py-3 px-5 rounded-full hover:bg-white hover:text-[#0A390C] transition-all duration-100">
           {name}
         </button>
       </Link>
