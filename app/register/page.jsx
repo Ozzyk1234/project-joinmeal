916c4160bef09592d7df0ff5ece96a38e7807812
Ozzyk1234 <bieleckioskar@gmail.com>
2024-03-15 15:26:19 +0100

    asd

--- app/register/page.jsx
+++ app/register/page.jsx
@@ -28,7 +28,7 @@ export default function RegisterPage() {
     }
 
     try {
-      const res = await fetch("http://localhost:3000/api/register", {
+      const res = await fetch("/api/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, email, password }),
