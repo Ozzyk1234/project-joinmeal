7e3c0cca70e082f8014fcaf8496668d81011d502
Ozzyk1234 <bieleckioskar@gmail.com>
2024-03-24 20:16:12 +0100

    asd

--- app/api/userdetails/[userId]/route.js
+++
@@ -1,8 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-import { NextResponse } from "next/server";
-
-const prisma = new PrismaClient();
-
-const userDetails = async (req) => {};
-
-export { userDetails as GET };
