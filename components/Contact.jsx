64036b1ff4d3601ede9a22ab575fdfcdba1eb0ee
Oskar Bielecki <Cookiemckfc@gmail.com>
2024-04-06 02:50:35 +0200

    asd

---
+++ components/Contact.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Contact() {
+  return (
+    <>
+      <div id="contact" className="-mt-40" />
+      <div className="w-full h-fit mt-40">
+        <h1 className="text-center text-5xl">Kontakt</h1>
+      </div>
+    </>
+  );
+}
--- components/Services.jsx
+++ components/Services.jsx
@@ -8,7 +8,7 @@ export default function Services() {
         <h1 className="text-center text-5xl">Usługi</h1>
         <div className="flex flex-row mt-16 w-[80%] mx-auto h-fit justify-between p-9">
           <motion.div
-            initial={{ opacity: 0, y: 200 }}
+            initial={{ opacity: 0, y: 100 }}
             whileInView={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.5, delay: 0.2 }}
             viewport={{ once: true }}
@@ -32,7 +32,7 @@ export default function Services() {
           </motion.div>
           <motion.div
             viewport={{ once: true }}
-            initial={{ opacity: 0, y: 200 }}
+            initial={{ opacity: 0, y: 100 }}
             whileInView={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.5, delay: 0.4 }}
             className="text-2xl border-[1px] border-gray-400 bg-white p-9 rounded-lg w-[28%] h-[200px] flex flex-col justify-center items-center"
@@ -54,7 +54,7 @@ export default function Services() {
             </motion.div>
           </motion.div>
           <motion.div
-            initial={{ opacity: 0, y: 200 }}
+            initial={{ opacity: 0, y: 100 }}
             whileInView={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.5, delay: 0.6 }}
             viewport={{ once: true }}
