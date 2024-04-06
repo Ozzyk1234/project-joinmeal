50c78551b61fb06e42ad6d5e834e727ac287e0ef
Oskar Bielecki <Cookiemckfc@gmail.com>
2024-04-06 03:12:08 +0200

    ppp

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -51,7 +51,7 @@ const Hero = () => {
           <motion.div
             initial={{ opacity: 0, y: 50 }}
             animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 1, delay: 1 }}
+            transition={{ duration: 1, delay: 1.5 }}
           >
             <Image
               src="/tlo.png"
