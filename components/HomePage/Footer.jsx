cbb193f03b6c8a8004629e498cb3da15b7afe724
Oskar Bielecki <Cookiemckfc@gmail.com>
2024-04-07 14:32:58 +0200

    asd

--- components/HomePage/Footer.jsx
+++ components/HomePage/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <>
-      <footer className="footer bg-gray-300 w-full">
+      <footer className="footer bg-gray-300 w-full mt-9">
         <p className="text-center">
           &copy; {new Date().getFullYear()} JoinMeal. Wszelkie prawa
           zastrzeżone.
