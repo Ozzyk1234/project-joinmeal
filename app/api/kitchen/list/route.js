ec27333ad06526c7f96a88c84f9b9ac9258261ec
Ozzyk1234 <bieleckioskar@gmail.com>
2024-04-16 20:45:48 +0200

    asd

--- app/api/kitchen/list/route.js
+++ app/api/kitchen/list/route.js
@@ -2,18 +2,6 @@
 
 import { PrismaClient } from "@prisma/client";
 import { NextResponse } from "next/server";
-<<<<<<< HEAD
-const { getKitchenList } = require("../slots");
-
-const GET = async (req, res) => {
-  const allKitchen = await getKitchenList();
-  if (allKitchen.length > 0) {
-    return NextResponse.json(allKitchen);
-  } else {
-    return NextResponse.json(
-      { message: "Blad pobierania kuchni" },
-=======
-
 const prisma = new PrismaClient();
 
 const GET = async (req, res) => {
@@ -28,7 +16,7 @@ const GET = async (req, res) => {
     console.error("Błąd:", error);
     return new NextResponse(
       { message: "Wystąpił błąd podczas pobierania danych z bazy." },
->>>>>>> 1b4c3d6e09f9709fd1b24a90bb3a500b6b7e2159
+
       { status: 500 }
     );
   }
