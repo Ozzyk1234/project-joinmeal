8cbcad9e350e669897189f763d26665cb9abf192
TaawII <91288629+TaawII@users.noreply.github.com>
2024-05-21 22:45:48 +0200

    Fix board
    poprawa wyswietlania ogloszen

--- app/api/board/showAll/route.js
+++ app/api/board/showAll/route.js
@@ -4,13 +4,27 @@ import { NextResponse } from "next/server";
 const prisma = new PrismaClient();
 
 const GET = async (req, { params }) => {
-    const allItems = await prisma.board.findMany();
+  try {
+    const allItems = await prisma.board.findMany({
+      orderBy: {
+        createdAt: 'desc',
+      },
+    });
 
     if (allItems.length > 0) {
       return NextResponse.json(allItems);
     } else {
       return NextResponse.json([]);
     }
+  } catch (error) {
+    console.error("Błąd podczas dodawania nowego ogłoszenia (board):", error);
+    return new NextResponse(
+      { message: "Wystąpił błąd podczas pobierania ogłoszen" },
+      { status: 500 }
+    );
+  } finally {
+    await prisma.$disconnect();
+  }
 }
 
 export { GET };
