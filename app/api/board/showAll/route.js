cf9541f5d1324b5bf7eb70ee37f74003d28b98ab
Ozzyk1234 <bieleckioskar@gmail.com>
2024-05-22 00:29:02 +0200

    asd

--- app/api/board/showAll/route.js
+++ app/api/board/showAll/route.js
@@ -3,12 +3,7 @@ import { NextResponse } from "next/server";
 
 const prisma = new PrismaClient();
 
-<<<<<<< HEAD
 const GET = async () => {
-=======
-const GET = async (req, res) => {
-  let allItems = [];
->>>>>>> 2c5d83fff9c45776e280eea9cfe29a1ed548ca92
   try {
     let allItems = await prisma.board.findMany({
       orderBy: {
@@ -17,15 +12,11 @@ const GET = async (req, res) => {
     });
 
     if (allItems.length > 0) {
-<<<<<<< HEAD
-      return NextResponse.json(allItems);
-=======
       return NextResponse.json(allItems, {
         headers: {
           "Cache-Control": "no-cache",
         },
       });
->>>>>>> 2c5d83fff9c45776e280eea9cfe29a1ed548ca92
     } else {
       return NextResponse.json([], {
         headers: {
